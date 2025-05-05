b14389b6a355135c3050fe4d67e6e8d6eadcb827
André Augusto Bortoli <andre@bortoli.phd>
2025-05-05 04:33:50 -0300

    feat: add monitor-interfaces to upload.sh

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 docker cp 10-mist-cni.conf kind-control-plane:/etc/cni/net.d/
 docker cp mist-cni kind-control-plane:/opt/cni/bin/
 docker cp tracing-bin kind-control-plane:/usr/local/bin/tracing
+docker cp monitor-interfaces.sh kind-control-plane:/usr/local/bin/monitor-interfaces
